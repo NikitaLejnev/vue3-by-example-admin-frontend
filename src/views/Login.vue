0a6606ea3c3cf93882eb3f016f47e22eb7d47d55
NikitaLejnev <catassasin331@gmail.com>
2022-01-26 13:32:29 +0300

    Added Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <Form @submit="onSubmit" :validation-schema="schema">
+    <div class="p-col-12">
+      <h1>Admin Log In</h1>
+    </div>
+
+    <Field v-slot="{ field, errors }" v-model="username" name="username">
+      <div class="p-col-12">
+        <div class="p-inputgroup">
+          <InputText
+            placeholder="Username"
+            :class="{ 'p-invalid': errors.length > 0 }"
+            v-bind="field"
+          />
+        </div>
+        <small class="p-error" v-if="errors.length > 0">Username is invalid.</small>
+      </div>
+    </Field>
+  </Form>
+</template>
