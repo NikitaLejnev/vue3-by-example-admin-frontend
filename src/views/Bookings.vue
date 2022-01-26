62efe23610524e3bdd62474d051e80c80bd4d1af
NikitaLejnev <catassasin331@gmail.com>
2022-01-26 09:36:10 +0300

    Added Bookings.vue

---
+++ src/views/Bookings.vue
@@ -0,0 +1,25 @@
+<template>
+  <TopBar title="Manage Bookings" />
+  <div class="p-col-12">
+    <h1>Manage Bookings</h1>
+  </div>
+  <div class="p-col-12">
+    <Card v-for="b of bookings" :key="b.id">
+      <template #title>{{ b.name }}</template>
+      <template #content>
+        <p>Address: {{ b.address }}</p>
+        <p>Description: {{ b.description }}</p>
+        <p>Start Date: {{ b.start_date }}</p>
+        <p>End Date: {{ b.end_date }}</p>
+      </template>
+      <template #footer>
+        <Button
+          icon="pi pi-times"
+          label="Cancel"
+          class="p-button-secondary"
+          @click="deleteBooking(b.id)"
+        />
+      </template>
+    </Card>
+  </div>
+</template>
