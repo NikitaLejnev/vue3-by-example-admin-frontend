74b5b45b21a1f18e057be4f2978f93f2c450c0cb
NikitaLejnev <catassasin331@gmail.com>
2022-01-26 13:58:42 +0300

    Added <script> to Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,3 +34,42 @@
     </div>
   </Form>
 </template>
+
+<script>
+import * as yup from "yup";
+import axios from "axios";
+import { APIURL } from "@/constants";
+
+const schema = yup.object().shape({
+  username: yup.string().required(),
+  password: yup.string().required(),
+});
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      schema,
+    };
+  },
+  methods: {
+    async onSubmit(values) {
+      const { username, password } = values;
+      try {
+        const {
+          data: { token },
+        } = await axios.post(`${APIURL}/users/login`, {
+          username,
+          password,
+        });
+        localStorage.setItem("token", token);
+        this.$router.push("/bookings");
+      } catch (error) {
+        alert("Login failed");
+      }
+    },
+  },
+};
+</script>
