1d975f22ff44dc0f77f5976e16b2045f6df84cdb
NikitaLejnev <catassasin331@gmail.com>
2022-01-26 08:33:06 +0300

    Added description form to CatalogForm.vue

---
+++ src/components/CatalogForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <Form @submit="onSubmit" :validation-schema="schema">
+    <Field v-slot="{ field, errors }" v-model="name" name="name">
+      <div class="p-col-12">
+        <div class="p-inputgroup">
+          <InputText placeholder="Name" :class="{ 'p-invalid': errors.length > 0 }" v-bind="field" />
+        </div>
+        <small class="p-error" v-if="errors.length > 0">Name is invalid.</small>
+      </div>
+    </Field>
+    <Field v-slot="{ field, errors }" v-model="description" name="description">
+      <div class="p-col-12">
+        <div class="p-inputgroup">
+          <Textarea
+            placeholder="Description"
+            :class="{ 'p-invalid': errors.length > 0 }"
+            v-bind="field"
+          />
+        </div>
+        <small class="p-error" v-if="errors.length > 0">Description is invalid</small>
+      </div>
+    </Field>
+  </Form>
+</template>
